d12a0c41b753e675fa493aacfb3044a4e836d2fc
sassoni <chrysanthi.n@gmail.com>
2014-05-12 23:39:42 -0400

    Test form

--- helloworld.py
+++ helloworld.py
@@ -2,12 +2,18 @@
 
 import webapp2
 
+form="""
+<form action="http://www.google.com/search">
+    <input name="q">
+    <input type="submit">
+</form>
+"""
 
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+        #self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write(form)
 
 
 application = webapp2.WSGIApplication([
