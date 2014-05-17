3943501d9d7a7e8bcb280fe046a7847dee8c4280
sassoni <chrysanthi.n@gmail.com>
2014-05-16 20:25:09 -0400

    Birthday validation

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,23 @@
 """A simple webapp2 server."""
 
 import webapp2
+import validdate
 
 form="""
-<form method="post" action="/testform">
-    <input name="q">
+<form method="post">
+    Birthday:
+    <br>
+    <label> Month
+    <input type="text" name="month">
+    </label>
+    <label> Day
+    <input type="text" name="day">
+    </label>
+    <label> Year
+    <input type="text" name="year">
+    </label>
+    <br>
+    <br>
     <input type="submit">
 </form>
 """
@@ -12,16 +25,15 @@ form="""
 class MainPage(webapp2.RequestHandler):
     def get(self):
         #self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write(form)
-
-class TestHandler(webapp2.RequestHandler):
+        self.response.out.write(form)
     def post(self):
-        #q = self.request.get("q")
-        #self.response.out.write(q)
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write(self.request)
+        birthday_month = validdate.valid_month(self.request.get('month'))
+        birthday_day = validdate.valid_day(self.request.get('day'))
+        birthday_year = validdate.valid_year(self.request.get('year'))
         
-application = webapp2.WSGIApplication([
-    ('/', MainPage),
-    ('/testform', TestHandler)
-], debug=True)
+        if (birthday_month and birthday_day and birthday_year):
+           self.response.out.write("This is a valid date!")
+        else:
+            self.response.out.write("Sorry, the date is not valid!")
+            
+application = webapp2.WSGIApplication([('/', MainPage)], debug=True)
