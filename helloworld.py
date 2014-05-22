b36404490aa05ce7dc96d0585d6aa162e843da8e
sassoni <chrysanthi.n@gmail.com>
2014-05-21 22:05:18 -0400

    adding jinja

--- helloworld.py
+++ helloworld.py
@@ -1,28 +1,30 @@
 """A simple webapp2 server."""
+import os 
 
+import jinja2
 import webapp2
 import sutil as s
 
-form="""
-<form method="post">
-    Birthday:
-    <br>
-    <label> Month <input type="text" name="month" value="%(month)s"> </label>
-    <label> Day <input type="text" name="day" value="%(day)s"> </label>
-    <label> Year <input type="text" name="year" value="%(year)s"> </label>
-    <div style="color:red">%(error)s</div>
-    <br>
-    <br>
-    <input type="submit">
-</form>
-"""
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
 
 class MainPage(webapp2.RequestHandler):
-    def print_form(self, error="", month="", day="", year=""):
-        self.response.out.write(form % {"error": error, "month": s.escape_html(month), "day": s.escape_html(day), "year": s.escape_html(year)})
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+        
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+        
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+    #def print_form(self, error="", month="", day="", year=""):
+        #self.response.out.write(form % {"error": error, "month": s.escape_html(month), "day": s.escape_html(day), "year": s.escape_html(year)})
  
     def get(self):
-        self.print_form()
+        self.render("birthday_form.html")
+        #self.print_form()
     
     def post(self):
         birthday_month = self.request.get('month')
@@ -30,8 +32,12 @@ class MainPage(webapp2.RequestHandler):
         birthday_year = self.request.get('year')
         
         if (s.valid_month(birthday_month) and s.valid_day(birthday_day) and s.valid_year(birthday_year)):
-           self.response.out.write("This is a valid date!")
+           self.redirect("/thanks")
         else:
            self.print_form("Sorry, the date is not valid!", birthday_month, birthday_day, birthday_year)
+           
+class ThanksHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.out.write("This is a valid date!")
             
-application = webapp2.WSGIApplication([('/', MainPage)], debug=True)
+application = webapp2.WSGIApplication([('/', MainPage), ('/thanks', ThanksHandler)], debug=True)
